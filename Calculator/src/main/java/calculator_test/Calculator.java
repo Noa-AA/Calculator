898d432a741798d095bc6d734b9e1ed9fb60d909
namhyanghee <namhyanghee@218.38.137.27>
2022-09-27 00:34:25 +0900

    220927 - button

--- Calculator/src/main/java/calculator_test/Calculator.java
+++ Calculator/src/main/java/calculator_test/Calculator.java
@@ -2,26 +2,75 @@ package calculator_test;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Calculator extends JFrame {
 	
 	private JTextField inputSpace;
 	
+	private String num = ""; //계산식에 사용될 숫자 num
+	
+	private ArrayList<String> equation = new ArrayList<String>(); //기능 구현: 숫자와 연산 기호 구분
+	
 	public Calculator() {	
 		
 		setLayout(null);	// 기본 레이아웃 설정
 		
+		//JTextField 생성
 		inputSpace = new JTextField(); //빈 공간 생성
 		inputSpace.setEditable(false); //편집 불가(버튼만 사용가능)
 		inputSpace.setBackground(Color.WHITE); //배경: 흰색
 		inputSpace.setHorizontalAlignment(JTextField.RIGHT); //정렬: 오른쪽
-		inputSpace.setFont(new Font("ginger", Font.BOLD, 50)); //글씨체 설정
-		inputSpace.setBounds(8, 10, 270, 70); //위치, 크기 지정(위치:x, y / 크기:270, 70) )
+		inputSpace.setFont(new Font("Arial", Font.BOLD, 50)); //글씨체 설정
+		inputSpace.setBounds(8, 10, 270, 70); //위치, 크기 지정(위치:x, y / 크기:270, 70) 
+		
+		
+		//버튼 생성
+		JPanel buttonPanel = new JPanel();
+		
+		//레이아웃 지정 - GridLayout 사용
+		buttonPanel.setLayout(new GridLayout(4, 5, 10, 10)); //GridLayout(가로 칸, 세로 칸, 좌우간격, 상하간격)
+		
+		//버튼 위치, 크기 지정
+		buttonPanel.setBounds(8, 90, 270, 235);
+		
+		//버튼 내용 배열에 저장
+		String button_names[] = { "C", "√", "%", "7", "8", "9", "×", "4", "5", "6", "-", "1", "2", "3", "+", "0", "00", ".", "=" };
+		
+		JButton buttons[] = new JButton[button_names.length]; //버튼 배열
+		
+		//배열을 통한 버튼 생성
+		for (int i = 0; i < button_names.length; i++) {
+			
+			buttons[i] = new JButton(button_names[i]);
+			
+			buttons[i].setFont(new Font("Arial", Font.BOLD, 20)); //글씨체
+			
+			
+			if (button_names[i] == "C") buttons[i].setBackground(Color.RED); //버튼 색 지정
+			
+			else if ((i >= 4 && i <= 6) || (i >= 8 && i <= 10) || (i >= 12 && i <= 14)) buttons[i].setBackground(Color.BLACK);
+			
+			else buttons[i].setBackground(Color.GRAY);
+			
+			
+			buttons[i].setForeground(Color.WHITE); //글자색 지정
+			
+			
+			buttons[i].setBorderPainted(false); //테두리 없애기
+			
+		}
+		
 		
 		add(inputSpace);
+		add(buttonPanel);
+		
 		
 		//계산기 창의 제목, 크기 등 요소 지정
 		setTitle("Calculator");	//제목 지정
